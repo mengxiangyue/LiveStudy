2c576b4b47befe0bcb08633c24510730ca9e7501
xiangyue.meng <xiangyue.meng@wenba100.com>
2016-07-13 22:08:43 +0800

    创建工程

---
+++ LiveStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveStudy
+//
+//  Created by wenba201600164 on 16/7/13.
+//  Copyright © 2016年 wenba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveStudy
+//
+//  Created by wenba201600164 on 16/7/13.
+//  Copyright © 2016年 wenba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
